60447baa03f69132d852b6cbdb66edf9fe4c8b1a
Abraham Airende <aoairende@gmail.com>
2023-07-13 00:35:02 -0700

    Add

---
+++ add.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point.
+ *
+ * Description: Sum of two integers.
+ *
+ * Return: 0.
+ *
+ */
+
+int main(void)
+{
+	int a = 3;
+	int b = 5;
+	int sum = a + b;
+
+	printf("Sum is %d\n', sum);
+
+	return 0;
+}
