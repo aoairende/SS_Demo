2a920e156b87218db664300620d67b7761f7d5bf
Abraham Airende <aoairende@gmail.com>
2023-07-18 03:19:28 -0700

    Child process

--- fork.c
+++ fork.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
 #include <unistd.h>
 
+/**
+ *main - Entry point.
+ *
+ * Description: Child process.
+ *
+ * Return: 0 if successful, -1 if unsuccessful.
+ */
+
 int main(void)
 {
 	pid_t pid;
 
 	printf("Before fork i was one\n");
 
-		fork();
+		pid = fork();
 
 		if (pid == -1)
 		{
